38130647a528c664a783844c94dc97eb5babe649
Eugene-Khonich <ehonich@gmail.com>
2024-11-20 22:56:47 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 import './style.css'
+type Errors = {
+  email?: string[];
+  firstName?: string[];
+  lastName?: string[];
+  phone?: string[];
+};
+
+type Form = {
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  phone: string | null;
+  errors: Errors;
+};
+
+// Реалізуйте Params так, 
+// щоб унеможливити поле 'errors' з типу Form
+type Params = Omit<Form, 'errors'>;
